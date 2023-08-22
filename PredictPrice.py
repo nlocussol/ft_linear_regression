085357aa41e0d25bd010b26b0b06db2a56ec0153
Nathan Locussol <nlocusso@1C6.42angouleme.fr>
2023-08-22 15:11:11 +0200

    faut que je clean

--- PredictPrice.py
+++ PredictPrice.py
@@ -1,6 +1,18 @@
+import pandas as pd
+import os
+
 def main():
     try:
-        ok
+        if os.path.isfile("./theta.csv"):
+            theta_data = pd.read_csv("./theta.csv")
+            last_row = theta_data.iloc[-1]
+            theta0 = last_row['theta0']
+            theta1 = last_row['theta1']
+        else:
+            theta0, theta1 = 0, 0
+            
+        mileage = input("Enter a mileage (in km): ")
+        print(f"This car worth {theta0 + theta1 * float(mileage):.0f} euro")
     except Exception as e:
         print(f"Error: {e}")
 
--- TrainModel.py
+++ TrainModel.py
@@ -1,38 +1,53 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-
+import csv
 
 def main():
     try:
-        data = pd.read_csv('data.csv')
-        X = data.km.values
-        y = data.price.values
+        with open('theta.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            field = ["theta0", "theta1"]
+            writer.writerow(field)
 
-        n = len(X)
+        
+            data = pd.read_csv('data.csv')
+            X = data.km.values
+            y = data.price.values
 
-        normalized_X = X / np.linalg.norm(X)
-        normalized_y = y / np.linalg.norm(y)
+            n = len(X)
 
-        theta0 = 0
-        theta1 = 0
+            normalized_X = X / np.linalg.norm(X)
+            normalized_y = y / np.linalg.norm(y)
 
-        lr = 0.5
-        for _ in range(1000):
-            sigma_theta0 = 0
-            sigma_theta1 = 0
-            for i in range(n):
-                sigma_theta0 += (theta0 + theta1 * normalized_X[i]) - normalized_y[i]
-                sigma_theta1 += ((theta0 + theta1 * normalized_X[i]) - normalized_y[i]) * normalized_X[i]
-            theta0 -= lr * (1 / n) * sigma_theta0
-            theta1 -= lr * (1 / n) * sigma_theta1
+            lr = 0.5
 
-        formula = (theta0 + theta1 * normalized_X) * np.linalg.norm(y)
-        plt.scatter(X, y)
-        plt.plot(X, formula, c='r')
-        plt.show()
+            theta0 = 0
+            theta1 = 0
 
+            for _ in range(1000):
+                sigma_theta0 = 0
+                sigma_theta1 = 0
+
+                for i in range(n):
+                    sigma_theta0 += (theta0 + theta1 * normalized_X[i]) - normalized_y[i]
+                    sigma_theta1 += ((theta0 + theta1 * normalized_X[i]) - normalized_y[i]) * normalized_X[i]
+
+                theta0 -= lr * (1 / n) * sigma_theta0
+                theta1 -= lr * (1 / n) * sigma_theta1
+                writer.writerow([theta0 * np.linalg.norm(y), theta1 * np.linalg.norm(X)])
+
+
+            theta0 = theta0 * np.linalg.norm(y)
+            theta1 = theta1 * (np.linalg.norm(y) / np.linalg.norm(X))
+
+            writer.writerow([theta0, theta1])
+            
+            linear_reg = theta0 + theta1 * X
+            
+            plt.scatter(X, y)
+            plt.plot(X, linear_reg, c='r')
+            plt.show()
         
     except Exception as e:
         print(f"Error: {e}")
